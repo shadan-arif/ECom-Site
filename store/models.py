2467e5709f12324fc87d1937437ee431f150e0fb
shadan-arif <shadanarif333@gmail.com>
2021-01-01 16:45:52 +0530

    Added Models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+# # Create your models here.
+# class Product(models.Model):
+#     name = models.CharField(max_length=50)
+#     price = models.IntegerField(default=0)
+#     description = models.CharField(max_length=200, default='' , null=True , blank=True)
+#     image = models.ImageField(upload_to='uploads/products/')
+
+
+
